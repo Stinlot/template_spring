f6bb79d728b1ac77166af79bcf605254fce81241
Stevann <stevann.tinlot@gmail.com>
2025-11-25 23:29:50 +0100

    Add hello world sysout

--- gradle/src/main/java/com/example/demo/DemoApplication.java
+++ gradle/src/main/java/com/example/demo/DemoApplication.java
@@ -3,11 +3,17 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.logging.Logger;
+
 @SpringBootApplication
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    private final static Logger LOGGER = Logger.getLogger(DemoApplication.class.getName());
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+
+        LOGGER.info("Hello World!");
+    }
 
 }
